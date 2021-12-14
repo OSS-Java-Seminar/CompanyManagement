eb3c37a934c5bba1a524313d8fdb530698c156d7
Marin Visković <marinviskovic89@gmail.com>
2021-12-14 17:38:23 +0100

    Added flyway script and fixed uuid

--- CompanyManagement/src/main/java/com/CompanyManagement/persistence/entities/Item.java
+++ CompanyManagement/src/main/java/com/CompanyManagement/persistence/entities/Item.java
@@ -2,6 +2,8 @@ package com.CompanyManagement.persistence.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.Set;
 import java.util.UUID;
@@ -12,6 +14,7 @@ import java.util.UUID;
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Type(type = "uuid-char")
     private UUID id;
 
     @Column(nullable = false)
---
+++ CompanyManagement/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,84 @@
+CREATE TABLE customer (
+  id char(36) NOT NULL,
+  customer_name VARCHAR(35) NOT NULL,
+  surname VARCHAR(35) NOT NULL,
+  oib VARCHAR(11) NOT NULL,
+  address VARCHAR(50) NULL,
+  telephone VARCHAR(35) NULL,
+  city VARCHAR(50) NULL,
+  CONSTRAINT pk_customer PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id char(36) NOT NULL,
+  employee_name VARCHAR(35) NOT NULL,
+  surname VARCHAR(35) NOT NULL,
+  oib BIGINT NOT NULL,
+  address VARCHAR(50) NULL,
+  role_id char(36) NOT NULL,
+  CONSTRAINT pk_employee PRIMARY KEY (id)
+);
+
+CREATE TABLE invoice (
+  id char(36) NOT NULL,
+  invoice_number INT NOT NULL,
+  total_amount FLOAT NOT NULL,
+  date_of_issue VARCHAR(23) NOT NULL,
+  due_date VARCHAR(23) NOT NULL,
+  vat FLOAT NOT NULL,
+  discount FLOAT NULL,
+  payment_status VARCHAR(10) NOT NULL,
+  payment_method VARCHAR(15) NOT NULL,
+  customer_id char(36) NOT NULL,
+  employee_id char(36) NOT NULL,
+  CONSTRAINT pk_invoice PRIMARY KEY (id)
+);
+
+CREATE TABLE invoice_items (
+  invoice_id char(36) NOT NULL,
+  items_id char(36) NOT NULL,
+  CONSTRAINT pk_invoice_items PRIMARY KEY (invoice_id, items_id)
+);
+
+CREATE TABLE item (
+  id char(36) NOT NULL,
+  price INT NOT NULL,
+  item_name VARCHAR(50) NOT NULL,
+  CONSTRAINT pk_item PRIMARY KEY (id)
+);
+
+CREATE TABLE item_invoices (
+  item_id char(36) NOT NULL,
+  invoices_id char(36) NOT NULL,
+  CONSTRAINT pk_item_invoices PRIMARY KEY (item_id, invoices_id)
+);
+
+CREATE TABLE user_role (
+  id char(36) NOT NULL,
+  role_name VARCHAR(20) NOT NULL,
+  CONSTRAINT pk_userrole PRIMARY KEY (id)
+);
+
+ALTER TABLE customer ADD CONSTRAINT uc_customer_oib UNIQUE (oib);
+
+ALTER TABLE employee ADD CONSTRAINT uc_employee_oib UNIQUE (oib);
+
+ALTER TABLE employee ADD CONSTRAINT uc_employee_role UNIQUE (role_id);
+
+ALTER TABLE invoice ADD CONSTRAINT uc_invoice_invoicenumber UNIQUE (invoice_number);
+
+ALTER TABLE user_role ADD CONSTRAINT uc_userrole_rolename UNIQUE (role_name);
+
+ALTER TABLE employee ADD CONSTRAINT FK_EMPLOYEE_ON_ROLE FOREIGN KEY (role_id) REFERENCES user_role (id);
+
+ALTER TABLE invoice ADD CONSTRAINT FK_INVOICE_ON_CUSTOMER FOREIGN KEY (customer_id) REFERENCES customer (id);
+
+ALTER TABLE invoice ADD CONSTRAINT FK_INVOICE_ON_EMPLOYEE FOREIGN KEY (employee_id) REFERENCES employee (id);
+
+ALTER TABLE invoice_items ADD CONSTRAINT fk_invite_on_invoice FOREIGN KEY (invoice_id) REFERENCES invoice (id);
+
+ALTER TABLE invoice_items ADD CONSTRAINT fk_invite_on_item FOREIGN KEY (items_id) REFERENCES item (id);
+
+ALTER TABLE item_invoices ADD CONSTRAINT fk_iteinv_on_invoice FOREIGN KEY (invoices_id) REFERENCES invoice (id);
+
+ALTER TABLE item_invoices ADD CONSTRAINT fk_iteinv_on_item FOREIGN KEY (item_id) REFERENCES item (id);
