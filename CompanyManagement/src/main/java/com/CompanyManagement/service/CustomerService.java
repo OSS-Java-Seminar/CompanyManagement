dca38ee195fad5e48330cd4a1ff5ef17672dce32
Vesna Simundic Bendic <77276348+Vesna0305@users.noreply.github.com>
2022-01-16 15:56:09 +0100

    Added CRUD for customer

--- CompanyManagement/src/main/java/com/CompanyManagement/service/CustomerService.java
+++ CompanyManagement/src/main/java/com/CompanyManagement/service/CustomerService.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @AllArgsConstructor
 @Component
 @Service
@@ -13,8 +16,32 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
-    public void TestCreate(Customer c) {
-        customerRepository.save(c);
+    public Customer createCustomer(Customer customer){
+        return customerRepository.save(customer);
     }
 
+    public List<Customer> getCustomers() {
+        return (List<Customer>) customerRepository.findAll();
+    }
+
+    public Customer findCustomerByOib(long oib) {
+        return customerRepository.findByOib(oib);
+    }
+
+    public void deleteCustomerById(UUID id) {
+        customerRepository.deleteById(id);
+    }
+
+    public void updateCustomer(Customer newcustomer, UUID id) {
+        var c = customerRepository.findById(id).orElse(null);
+
+        c.setCustomerName(newcustomer.getCustomerName());
+        c.setSurname(newcustomer.getSurname());
+        c.setOib(newcustomer.getOib());
+        c.setAddress(newcustomer.getAddress());
+        c.setTelephone(newcustomer.getTelephone());
+        c.setCity(newcustomer.getCity());
+
+        customerRepository.save(c);
+    }
 }
